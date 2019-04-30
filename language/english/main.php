b2ae1c6b4ec570963d4bc146127cfd6f9c50a089
Tad <tad0616@gmail.com>
2019-04-30 12:51:45 +0800

    加語系

--- language/english/main.php
+++ language/english/main.php
@@ -1,4 +1,5 @@
 <?php
+xoops_loadLanguage('main', 'tadtools');
 
 define('_TAD_NEED_TADTOOLS', "This module needs TadTools module. You can download TadTools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
 define('_MD_TADCAL_SMNAME2', 'Add Event');
--- language/tchinese_utf8/main.php
+++ language/tchinese_utf8/main.php
@@ -1,5 +1,5 @@
 <?php
-//需加入模組語系
+xoops_loadLanguage('main', 'tadtools');
 define('_TAD_NEED_TADTOOLS', '需要 tadtools 模組，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
 define('_MD_TADCAL_SMNAME2', '新增事件');
 define('_MD_TADCAL_SMNAME3', '下載簡曆');
